e7879858cfe255f966805c968d1af5d148cda56c
d3m0l15h <davicmax123@gmail.com>
2022-11-03 21:56:12 +0700

    subCommand Event Command

---
+++ Commands/Developer/Reload/EventSub.js
@@ -0,0 +1,17 @@
+const { ChatInputCommandInteraction, Client } = require('discord.js');
+const { loadEvents } = require("../../../Structures/Handlers/EventHandler");
+
+module.exports = {
+    subCommand: "reload.commands",
+    /**
+     * 
+     * @param {ChatInputCommandInteraction} interaction 
+     * @param {Client} client 
+     */
+    execute(interaction, client) {
+        for(const [key, value] of client.events)
+        client.removeListener(`${key}`, value, true);
+        loadEvents(client);
+        interaction.reply({content: "Reloaded Events", ephemeral: true});
+    }
+}
