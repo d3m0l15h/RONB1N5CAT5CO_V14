a7dbddfe12d59f65259b50a9c2f7a2ce0bad99a3
d3m0l15h <davicmax123@gmail.com>
2022-11-03 10:30:03 +0700

    Connect MongoDB

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+////////////////////////////////
+const Token = process.env.Token;
+const Database = process.env.Database;
+////////////////////////////////
+const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember} = Partials;
+
+const client = new Client({ 
+    intents: [Guilds, GuildMembers, GuildMessages],
+    partials: [User, Message, GuildMember, ThreadMember] 
+});
+////////////////////////////////
+client.commands = new Collection();
+client.events = new Collection();
+////////////////////////////////
+const { loadEvents } = require('./Structures/Handlers/EventHandler');
+loadEvents(client);
+////////////////////////////////
+const { connect } = require('mongoose');
+connect(Database, {   
+}).then(() => console.log('Connected to the database.'));
+
+client.login(Token);
