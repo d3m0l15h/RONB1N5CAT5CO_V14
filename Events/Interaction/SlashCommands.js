96432e20afe4847ada83de813da1c21d2d56a1d4
d3m0l15h <davicmax123@gmail.com>
2022-11-20 16:11:30 +0700

    Button Interaction

--- Events/Interaction/SlashCommands.js
+++ Events/Interaction/SlashCommands.js
@@ -1,37 +1,48 @@
-const { ChatInputCommandInteraction } = require('discord.js');
-
 module.exports = {
     name: "interactionCreate",
-    /** 
-     * @param {ChatInputCommandInteraction} interaction
-     * */ 
+
     execute(interaction, client) {
-        if(!interaction.isChatInputCommand()) return;
+        if(interaction.isChatInputCommand()) {
 
-        const command = client.commands.get(interaction.commandName);
-        
-        if(!command)
-        return interaction.reply({
-            content: "This command is outdated.",
-            ephemeral: true
-        });
-
-        if(command.developer && interaction.user.id !== "390075791448342529")
-        return interaction.reply({
-            content: "This command is available for the developer.",
-            ephemeral: true
-        });
-
-        const subCommand = interaction.options.getSubcommand(false);
-        
-        if(subCommand) {
-            const subCommandFile = client.subCommands.get(`${interaction.commandName}.${subCommand}`);
-            if(!subCommandFile) 
+            const command = client.commands.get(interaction.commandName);
+            
+            if(!command)
             return interaction.reply({
-                content: "This subcommand is outdated.",
+                content: "This command is outdated.",
                 ephemeral: true
             });
-            subCommandFile.execute(interaction, client);
-        } else command.execute(interaction, client);
+
+            if(command.developer && interaction.user.id !== "390075791448342529")
+            return interaction.reply({
+                content: "This command is available for the developer.",
+                ephemeral: true
+            });
+
+            const subCommand = interaction.options.getSubcommand(false);
+
+            if(subCommand) {
+                const subCommandFile = client.subCommands.get(`${interaction.commandName}.${subCommand}`);
+                if(!subCommandFile) 
+                return interaction.reply({
+                    content: "This subcommand is outdated.",
+                    ephemeral: true
+                });
+                subCommandFile.execute(interaction, client);
+            } else command.execute(interaction, client);
+
+        } else if(interaction.isButton()) {
+            try {
+            const Button = client.buttons.get(interaction.customId);
+
+            if(Button.permission && !interaction.member.permissions.has(Button.permission))
+                return interaction.reply({content: "You don't have permission to use this button", ephemeral: true});
+
+            if(Button.ownerOnly && interaction.member.id !== interaction.guild.ownerId)
+                return interaction.reply({content: "You don't have permission to use this button", ephemeral: true});
+
+            Button.execute(interaction, client);
+            } catch(err) {}
+        }
+        else return;
     }
 }
