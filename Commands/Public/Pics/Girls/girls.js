8551e79df41e9206e18a8c87e9df46ba8c19ab24
d3m0l15h <davicmax123@gmail.com>
2022-12-01 11:09:10 +0700

    Send beautiful girl images

---
+++ Commands/Public/Pics/Girls/girls.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const girlcollections_key = process.env.girlcollections_key;
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    subCommand: "pics.girls",
+    
+    async execute(interaction, client) { 
+        let randOffset = Math.floor(Math.random() * (956 / 20))
+
+        const data = await fetch(`https://api.tumblr.com/v2/blog/gaixinhchonloc.com/posts/photo?api_key=${girlcollections_key}&limit=20&offset=${randOffset}`).then(res => res.json());
+
+        let randImg = Math.floor(Math.random() * 20)
+
+        const girls = new EmbedBuilder()
+                        .setColor('Blurple')
+                        .setImage(data.response.posts[randImg].photos[0].original_size.url)
+
+        return interaction.reply({embeds: [girls]})
+    }
+}
+
+
